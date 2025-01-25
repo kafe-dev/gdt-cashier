b36e99e0db767bff88d6b59188df25f8a5763efe
Hải Phạm <haidev6199@gmail.com>
2025-01-25 16:55:09 +0700

    haipv: [Site Index Paygate] - done

---
+++ app/Services/DataTables/PaygateDataTable.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\DataTables;
+
+use App\Models\Filters\PaygateFilter;
+use App\Models\Paygate;
+use App\Services\DataTables\Transformers\PaygateTransformer;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Column;
+
+/**
+ * Class UserDataTable.
+ *
+ * This class provides a DataTables integration for the User model.
+ */
+class PaygateDataTable extends BaseDataTable {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected string $tableId = 'paygate-table';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected string $createUrl = 'app.paygate.create';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected string $exportFileName = 'Paygates_';
+
+    /**
+     * Return the query builder instance to be processed by DataTables.
+     */
+    public function query(Paygate $model): QueryBuilder {
+        return $model->newQuery();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dataTable(): EloquentDataTable {
+        $dataTable = (new EloquentDataTable(new Paygate))->setTransformer(PaygateTransformer::class)->setRowId('id')->escapeColumns([])->rawColumns(['action']);
+        return PaygateFilter::perform($dataTable);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getColumns(): array {
+        return [
+            Column::make([
+                'data'  => 'id',
+                'title' => 'ID',
+            ])->addClass('x-id'),
+            Column::make('url')->searchPanes()->addClass('x-searchable'),
+            //Column::make('api_data')->addClass('x-searchable'),
+            //Column::make('vps_data')->addClass('x-searchable'),
+            Column::make('type')->searchPanes()->addClass('x-searchable'),
+            Column::make('status')->searchPanes()->addClass('x-searchable'),
+            Column::make('limitation')->searchPanes()->addClass('x-searchable'),
+            Column::make('mode')->searchPanes()->addClass('x-searchable'),
+            Column::make('created_at')->searchPanes()->addClass('x-has-date-filter')->orderable(false),
+            Column::make('updated_at')->searchPanes()->addClass('x-has-date-filter')->orderable(false),
+        ];
+    }
+}
