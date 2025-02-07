3a9ad7a75efbac783ef8ceb30df2ffd21abe2ba6
hoepjhsha <hiepnguyen3624@gmail.com>
2025-02-07 18:10:18 +0700

    update: update breadcrumbs

--- routes/app/breadcrumbs/tracking.php
+++ routes/app/breadcrumbs/tracking.php
@@ -1,1 +1,17 @@
 <?php
+
+use App\Models\OrderTracking;
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// Dashboard > Manage Order Tracking
+Breadcrumbs::for('manage-order-tracking', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Manage Order Tracking', route('app.tracking.index'));
+});
+
+// Dashboard > Manage Order Tracking > Show Order Tracking
+Breadcrumbs::for('show-order-tracking', function (BreadcrumbTrail $trail, OrderTracking $orderTracking) {
+    $trail->parent('manage-order-tracking');
+    $trail->push('Show User', route('app.tracking.show', ['id' => $orderTracking->id]));
+});
