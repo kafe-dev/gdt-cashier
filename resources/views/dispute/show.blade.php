15375ee33449dae65ca887b1ae2799d77ac9d4e1
NTDzVEKNY <ntdgdeptrai@gmail.com>
2025-02-09 00:36:39 +0700

    Feat: acknowledge return

--- resources/views/dispute/show.blade.php
+++ resources/views/dispute/show.blade.php
@@ -77,7 +77,9 @@
                                                 <p>{{$message['content']}}</p>
                                                 @foreach($offers as $offer)
                                                     @if($offer['actor'] == 'BUYER' && $offer['offer_time'] === $message['time_posted'])
-                                                        <span class="@if($offer['event_type'] == "DENIED") text-danger @else text-success @endif" style="font-size: larger">Offer: {{ $offer['event_type'] }}</span>
+                                                        <span
+                                                            class="@if($offer['event_type'] == "DENIED") text-danger @else text-success @endif"
+                                                            style="font-size: larger">Offer: {{ $offer['event_type'] }}</span>
                                                     @endif
                                                 @endforeach
                                             </div>
@@ -121,13 +123,16 @@
                         <a class="dropdown-item" href="#">Escalate dispute to claim</a>
                         <a class="dropdown-item" href="#" data-bs-toggle="modal"
                            data-bs-target="#make-offer-dispute-modal">Make offer to resolve dispute</a>
-                        <a class="dropdown-item" href="#">Accept claim</a>
+                        <a class="dropdown-item" href="#" data-bs-toggle="modal"
+                           data-bs-target="#acknowledge-returned-dispute-modal">Accept claim</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-12"></div>
     </div>
+
+    {{--    BEGIN Send message modal--}}
     <div class="modal fade" id="send-message-dispute-modal" tabindex="-1"
          aria-labelledby="send-message-dispute-modal-label" aria-hidden="true" style="display: none;">
         <div class="modal-dialog" role="document">
@@ -141,12 +146,15 @@
                     <div class="row">
                         <form action="{{ route('app.dispute.send-message') }}" method="POST">
                             @csrf
-                            <input type="hidden" class="form-control" id="paygate_id" name="paygate_id" value="{{$paygate->id}}" required readonly>
-                            <input type="hidden" class="form-control" id="dispute_id" name="dispute_id" value="{{$dispute->id}}" required readonly>
+                            <input type="hidden" class="form-control" id="paygate_id" name="paygate_id"
+                                   value="{{$paygate->id}}" required readonly>
+                            <input type="hidden" class="form-control" id="dispute_id" name="dispute_id"
+                                   value="{{$dispute->id}}" required readonly>
 
                             <div class="form-group">
                                 <label for="dispute_id">Dispute Code:</label>
-                                <input type="text" class="form-control" id="dispute_code" name="dispute_code" value="{{$dispute_arr['dispute_id']??'N/A'}}" required readonly>
+                                <input type="text" class="form-control" id="dispute_code" name="dispute_code"
+                                       value="{{$dispute_arr['dispute_id']??'N/A'}}" required readonly>
                             </div>
 
                             <div class="form-group">
@@ -161,13 +169,15 @@
             </div><!--end modal-content-->
         </div><!--end modal-dialog-->
     </div>
+    {{--    End Send message modal--}}
+
 
     {{--    Begin Make Offer Modal --}}
     <div class="modal fade" id="make-offer-dispute-modal" tabindex="-1" aria-labelledby="make-offer-dispute-modal-label"
          aria-hidden="true" style="display: none;">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <form action="{{ route('app.dispute.makeOffer', $dispute->id) }}" method="POST">
+                <form action="{{ route('app.dispute.acknowledgeReturned', $dispute->id) }}" method="POST">
                     @csrf
                     <div class="modal-header">
                         <h6 class="modal-title m-0" id="make-offer-dispute-modal-label">Make offer to resolve
@@ -189,7 +199,7 @@
                             <div id="amount-section">
                                 <div class="mb-3">
                                     <label class="form-label">Amount <span class="text-danger">*</span></label>
-                                    <input type="number" name="amount" maxlength="32" min="0.01"  step="0.01"
+                                    <input type="number" name="amount" maxlength="32" min="0.01" step="0.01"
                                            class="form-control">
                                 </div>
 
@@ -217,7 +227,8 @@
                             <div id="return-address-section">
                                 <div class="mb-3">
                                     <label class="form-label">Address <span class="text-danger">*</span></label>
-                                    <input type="text" name="address" maxlength="300" class="form-control mb-2" placeholder="Address">
+                                    <input type="text" name="address" maxlength="300" class="form-control mb-2"
+                                           placeholder="Address">
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Country Code <span class="text-danger">*</span></label>
@@ -237,6 +248,77 @@
         </div><!--end modal-dialog-->
     </div>
     {{--    End Make Offer Modal--}}
+
+
+    {{--    Begin Acknowledge Returned Item Modal --}}
+    <div class="modal fade" id="acknowledge-returned-dispute-modal" tabindex="-1"
+         aria-labelledby="acknowledge-returned-dispute-modal-label"
+         aria-hidden="true" style="display: none;">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form action="{{ route('app.dispute.acknowledgeReturned', $dispute->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h6 class="modal-title m-0" id="acknowledge-returned-dispute-modal-label">Acknowledge Returned
+                            Item
+                            <h6>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                    </div><!--end modal-header-->
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="mb-3">
+                                <label for="note" class="form-label">Notes (optional)</label>
+                                <textarea id="acknowledge-returned-note" name="note" class="form-control" rows="3"
+                                          maxlength="2000"></textarea>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="item_status" class="form-label">Status</label>
+                                <select id="acknowledge-returned-item_status" name="item_status" class="form-select"
+                                        required>
+                                    <option value="NORMAL" selected>Item Received Normally</option>
+                                    <option value="ISSUE">Has Issues</option>
+                                </select>
+                            </div>
+                            <div id="acknowledge-returned-evidence-section">
+                                <div class="mb-3">
+                                    <label for="evidence_type" class="form-label">Evidence Type <span
+                                            class="text-danger">*</span></label>
+                                    <select id="acknowledge-returned-evidence_type" name="evidence_type"
+                                            class="form-select" required>
+                                        <option value="PROOF_OF_DAMAGE">Proof of Damage</option>
+                                        <option value="THIRDPARTY_PROOF_FOR_DAMAGE_OR_SIGNIFICANT_DIFFERENCE">
+                                            Third-Party Proof for Damage or Significant Difference
+                                        </option>
+                                        <option value="DECLARATION">Declaration</option>
+                                        <option value="PROOF_OF_MISSING_ITEMS">Proof of Missing Items</option>
+                                        <option value="PROOF_OF_EMPTY_PACKAGE_OR_DIFFERENT_ITEM">
+                                            Proof of Empty Package or Different Item
+                                        </option>
+                                        <option value="PROOF_OF_ITEM_NOT_RECEIVED">Proof of Item Not Received</option>
+                                    </select>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="documents" class="form-label">Upload Evidence Documents <span
+                                            class="text-danger">*</span></label>
+                                    <input type="file" id="acknowledge-returned-documents" name="documents[]"
+                                           class="form-control" multiple required accept="image/*,application/pdf" >
+                                </div>
+                            </div>
+
+                        </div><!--end row-->
+                    </div><!--end modal-body-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary btn-sm">Apply</button>
+                    </div><!--end modal-footer-->
+                </form>
+            </div><!--end modal-content-->
+        </div><!--end modal-dialog-->
+    </div>
+    {{--    End Acknowledge Returned Item Modal --}}
 @endsection
 
 @push('custom-scripts')
@@ -250,7 +332,7 @@
             const amountInput = modal.querySelector("input[name='amount']");
             const currencySelect = modal.querySelector("select[name='currency']")
             const addressInput = modal.querySelector("input[name='address']");
-            const countryCodeInput =  modal.querySelector("input[name='country_code']");
+            const countryCodeInput = modal.querySelector("input[name='country_code']");
 
             function updateFormFields() {
                 const selectedType = offerTypeSelect.value;
@@ -279,6 +361,38 @@
 
             // Cập nhật khi thay đổi Offer Type
             offerTypeSelect.addEventListener("change", updateFormFields);
+
+            // Gọi khi trang load lần đầu
+            updateFormFields();
+        });
+    </script>
+
+    <script>
+        // Acknowledge returned item
+        document.addEventListener("DOMContentLoaded", function () {
+            const modal = document.getElementById("acknowledge-returned-dispute-modal");
+            const itemStatusSelect = modal.querySelector("select[name='item_status']");
+            const evidenceSection = modal.querySelector("#acknowledge-returned-evidence-section");
+            const evidenceSelect = modal.querySelector("select[name='evidence_type']");
+            const fileInput = modal.querySelector("input[name='documents[]']");
+
+            function updateFormFields() {
+                const selectedStatus = itemStatusSelect.value;
+
+                evidenceSection.style.display = "none";
+                evidenceSelect.removeAttribute("required");
+                fileInput.removeAttribute("required");
+
+                // Has Issues
+                if (selectedStatus === "ISSUE") {
+                    evidenceSection.style.display = "block";
+                    evidenceSelect.setAttribute("required", "required");
+                    fileInput.setAttribute("required", "required");
+                }
+            }
+
+            // Cập nhật khi thay đổi Item Status
+            itemStatusSelect.addEventListener("change", updateFormFields);
 
             // Gọi khi trang load lần đầu
             updateFormFields();
