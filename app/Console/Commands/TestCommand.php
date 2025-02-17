5d67fffd1dbbebe239d7d7fcc37a5512ff022e2c
Hải Phạm <haidev6199@gmail.com>
2025-02-17 22:46:50 +0700

    haipv: [form provide evidence] - todo

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -6,12 +6,10 @@ use App\Helpers\Logs;
 use App\Models\Paygate;
 use App\Paygate\PayPalAPI;
 use DateTime;
-use DateTime;
 use Illuminate\Console\Command;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
-class TestCommand extends Command
-{
+class TestCommand extends Command {
 
     /**
      * The name and signature of the console command.
@@ -31,45 +29,58 @@ class TestCommand extends Command
      * Execute the console command.
      * @throws \Exception
      */
-    public function handle()
-    {
-        $paygate = Paygate::find(3);
-        $paypalApi = new PayPalAPI($paygate);
+    public function handle() {
+        $this->test();
+        die;
+        $paygate    = Paygate::find(3);
+        $paypalApi  = new PayPalAPI($paygate);
         $dispute_id = 'PP-R-WAS-10106358';
-
-//        $data = [
-//            'evidences' => [
-//                [
-//                    'evidence_type' => 'PROOF_OF_FULFILLMENT',
-//                    'evidence_info' => [
-//                        'tracking_info' => [
-//                            [
-//                                'carrier_name' => 'FEDEX',
-//                                'tracking_number' => '122533485',
-//                            ],
-//                        ],
-//                    ],
-//                    'notes' => 'Thông tin giao hàng được cung cấp.',
-//                    'documents'=>[],
-//                ],
-//            ],
-//        ];
-
-        $result = $paypalApi->test(
-            __DIR__.'/1202_1.jpg',
-            $dispute_id
-        );
-
-        echo '<pre>start-debug'.PHP_EOL;
-        print_r($result).PHP_EOL;
+        //        $data = [
+        //            'evidences' => [
+        //                [
+        //                    'evidence_type' => 'PROOF_OF_FULFILLMENT',
+        //                    'evidence_info' => [
+        //                        'tracking_info' => [
+        //                            [
+        //                                'carrier_name' => 'FEDEX',
+        //                                'tracking_number' => '122533485',
+        //                            ],
+        //                        ],
+        //                    ],
+        //                    'notes' => 'Thông tin giao hàng được cung cấp.',
+        //                    'documents'=>[],
+        //                ],
+        //            ],
+        //        ];
+        $result = $paypalApi->test(__DIR__ . '/1202_1.jpg', $dispute_id);
+        echo '<pre>start-debug' . PHP_EOL;
+        print_r($result) . PHP_EOL;
         die('--end--');
-
-
-
         //$result = $paypalApi->provideEvidence($dispute_id, $data);
         echo '<pre>start-debug' . PHP_EOL;
         print_r($result) . PHP_EOL;
         die('--end--');
+    }
 
+    public function test() {
+        $data = [
+            'evidences' => [
+                [
+                    'evidence_type' => 'PROOF_OF_FULFILLMENT',
+                    'evidence_info' => [
+                        'tracking_info' => [
+                            [
+                                'carrier_name'    => 'FEDEX',
+                                'tracking_number' => '122533485',
+                            ],
+                        ],
+                    ],
+                    'notes'         => '',
+                    'documents'     => [],
+                ],
+            ],
+        ];
+
+        echo json_encode($data);die;
     }
 }
